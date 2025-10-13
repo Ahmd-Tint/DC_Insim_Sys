82451418a70ef5d6e7b2e894c698e1e0fb28ed8c
Ahmd-Tint <inttni222@gmail.com>
2025-10-13 20:42:01 +0300

    Create keep_alive.js

---
+++ keep_alive.js
@@ -0,0 +1,15 @@
+// keep_alive.js
+const express = require('express');
+const server = express();
+
+server.all('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log('Server is Ready!');
+  });
+}
+
+module.exports = keepAlive;
